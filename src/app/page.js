d6ceca4e8ebabe7de85a0d365a6877bd0256a277
Jayesh Poduval <jayeshpoduval31@gmail.com>
2025-08-03 15:01:18 +1000

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,103 +1,120 @@
-import Image from "next/image";
+"use client";
+import { useState, useRef, useEffect } from "react";
+import {
+  AnimatePresence,
+  motion,
+  useMotionValue,
+  useMotionValueEvent,
+} from "motion/react";
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [isKnobHovered, setIsKnobHovered] = useState(false);
+  const [isKnobDragged, setIsKnobDragged] = useState(false);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+  // Angle display
+  const [knobAngleDisplay, setKnobAngleDisplay] = useState(0);
+
+  const knobAngle = useMotionValue(0);
+
+  // Subscribe to updates
+  useMotionValueEvent(knobAngle, "change", (latest) => {
+    setKnobAngleDisplay(Number(latest.toFixed(0)));
+  });
+
+  const knobRef = useRef();
+
+  useEffect(() => {
+    if (!isKnobDragged || !knobRef.current) return;
+
+    const handlePointerMove = (e) => {
+      const { clientX, clientY } = e;
+      const { width, left, height, top } =
+        knobRef.current.getBoundingClientRect();
+      const knobCenterX = left + width / 2;
+      const knobCenterY = top + height / 2;
+
+      const dx = clientX - knobCenterX; // x-angle
+      const dy = clientY - knobCenterY; // y-angle
+
+      const angleRadian = Math.atan2(dy, dx); // atan2 gives 360deg values
+      const angleDegree = angleRadian * (180 / Math.PI); // Convert radians to degrees
+
+      knobAngle.set(angleDegree);
+    };
+
+    const handlePointerUp = () => {
+      setIsKnobDragged(false);
+      setIsKnobHovered(false);
+      window.removeEventListener("pointerup", handlePointerUp);
+      window.removeEventListener("pointermove", handlePointerMove);
+    };
+
+    if (isKnobDragged) {
+      window.addEventListener("pointermove", handlePointerMove);
+      window.addEventListener("pointerup", handlePointerUp);
+    }
+
+    return () => {
+      window.removeEventListener("pointerup", handlePointerUp);
+      window.removeEventListener("pointermove", handlePointerMove);
+    };
+  }, [isKnobDragged]);
+
+  const handlePointerDown = () => {
+    setIsKnobDragged(true);
+    setTimeout(() => {
+      setIsKnobHovered(false);
+    }, 200);
+  };
+
+  return (
+    <section className="relative h-screen bg-background flex flex-col gap-8 items-center justify-center">
+      <motion.div
+        ref={knobRef}
+        className={`before:rounded-full ${
+          isKnobHovered || isKnobDragged ? "before:hidden" : "before:block"
+        } before:absolute before:-inset-2 before:border-2 active:cursor-grabbing before:border-foreground before:size-24  before:content-['']  size-20 relative cursor-grab relative z-20 rounded-full bg-foreground flex items-start justify-center`}
+        onPointerEnter={() => setIsKnobHovered(true)}
+        onPointerLeave={() => setIsKnobHovered(false)}
+        onPointerDown={handlePointerDown}
+        style={{
+          rotate: knobAngle,
+        }}
+        animate={{
+          scale: isKnobDragged || isKnobHovered ? 1.4 : 1,
+        }}
+        transition={{
+          type: "spring",
+          mass: 1.5,
+          stiffness: 580,
+          damping: 45,
+        }}
+        whileTap={{ scale: 1.25 }}
+      >
+        <div className="h-[calc(50%-2.5px)] rounded-full w-[5px] bg-white translate-y-[10%]"></div>
+        <AnimatePresence>
+          {isKnobDragged && (
+            <motion.div
+              className="h-[30px] w-[2px] bg-foreground absolute bottom-[calc(100%+10px)]"
+              initial={{ scale: 0.5, opacity: 0, transformOrigin: "bottom" }}
+              animate={{ scale: 1, opacity: 1 }}
+              transition={{
+                type: "spring",
+                mass: 1,
+                stiffness: 730,
+                damping: 49,
+              }}
+              exit={{
+                scale: 0.9,
+                opacity: 0,
+                transition: { bounce: 0.1, duration: 0.1 },
+              }}
+            >
+              <span className="select-none absolute bottom-10 font-semibold rounded-lg p-0.5 tabular-nums w-[50px] bg-foreground/10 -translate-x-[25px] flex justify-center">{`${knobAngleDisplay}°`}</span>
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </motion.div>
+    </section>
   );
 }
